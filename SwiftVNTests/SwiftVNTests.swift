749c507fe7f330410d6d618f2357bec0a26a2490
kru <kruniak@protonmail.com>
2024-09-28 00:09:10 +0200

    Initial Commit

---
+++ SwiftVNTests/SwiftVNTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftVNTests.swift
+//  SwiftVNTests
+//
+//  Created by Kru on 28/09/24.
+//
+
+import Testing
+@testable import SwiftVN
+
+struct SwiftVNTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
